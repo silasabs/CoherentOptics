8329311d966306988ef63c649f80364070b5dfdd
Silas João <silasroot321@gmail.com>
2024-05-26 17:28:35 -0300

    Organiza o diretório de funções

---
+++ algorithms/equalizaton.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+def next_power_of_2(n):
+    return 1 << (int(np.log2(n - 1)) + 1)
+
+def fft_convolution(x, h):
+    """ 
+    Obtem o produto de x e h no domínio da frequência.
+
+    Args:
+        x (np.array): sinal de entrada
+        h (np.array): coeficientes do filtro
+
+    Returns:
+        y np.array: sinal de saída após a convolução.
+    """
+    # Tamanho da saída
+    Ny = x.shape[0] + h.shape[0] - 1 
+
+    # Calcule as transformadas rápidas de Fourier
+    # dos sinais no domínio do tempo
+    X = np.fft.fft(x)
+    H = np.fft.fft(h)
+
+    # Realiza a convolução circular no domínio da frequência
+    Y = X * H
+
+    # Volta ao domínio do tempo
+    y = np.fft.ifft(Y)
+
+    # Corte o sinal para o comprimento de saida esperado
+    return y[:Ny]
+
+def overlap_save_convolution(x, h, B, NFFT=None):
+    """ Convolução Overlap-Save de x e h com comprimento de bloco B
+        
+        input:
+            x (np.array): signal de entrada.
+            h (np.array): coeficientes do filtro.
+            B (int)     : comprimento do bloco.
+            NFFT (int)  : comprimento do filtro.
+        output:
+            y np.array: sinal convoluído.
+    """
+
+    M = len(x)
+    N = len(h)
+
+    if NFFT is None:
+        NFFT = max(B, next_power_of_2(N))
+        
+    # Determina o número de blocos de entrada
+    num_input_blocks = np.ceil(M / B).astype(int) \
+                     + np.ceil(NFFT / B).astype(int) - 1
+
+    # realiza o zero padding ao sinal x para um múltiplo inteiro de B
+    xp = np.pad(x, (0, num_input_blocks*B - M), mode='constant', constant_values=0+0j)
+
+    output_size = num_input_blocks * B + N - 1
+    y = np.zeros((output_size,), dtype="complex")
+    
+    # Buffer de entrada
+    xw = np.zeros((NFFT,), dtype="complex")
+
+    # Convolução de todos os blocos
+    for n in range(num_input_blocks):
+        # Extraia o enésimo bloco de entrada
+        xb = xp[n*B:n*B+B]
+
+        # Janela deslizante da entrada
+        xw = np.roll(xw, -B)
+        xw[-B:] = xb
+
+        # Convolução rápida por FFT
+        u = fft_convolution(xw, h)
+
+        # Salve as amostras de saída válidas
+        y[n*B:n*B+B] = u[-B:]
+
+    y = y[:M+N-1]
+    
+    # Remove o atraso do filtro FIR
+    start = (N - 1) // 2
+    return y[start:start+M]
+
+def lms(u, d, taps, mu):
+    """ Least mean squares (LMS)
+    
+    Simples implementação do algoritmo LMS para filtragem adaptativa.
+
+    Args:
+        u (np.array): sinal de entrada unidimensional 
+        d (np.array): sinal de referência unidimensional
+        taps (int)  : número de coeficientes do filtro   
+        mu (float)  : tamanho do passo para o LMS
+
+    Returns:
+        tuple: 
+            - y np.array  : sinal de saída.
+            - e np.array  : sinal de erro.
+            - mse np.array: erro quadrático médio.
+            - w np.array  : coeficintes do filtro após a convergência.
+    """
+    # número de iterações para filtragem adaptativa
+    N = len(u) - taps + 1
+
+    y = np.zeros(N)     # saída do filtro
+    e = np.zeros(N)     # sinal de erro
+    w = np.zeros(taps)  # coeficientes iniciais do filtro.
+
+    squaredError = np.zeros(N)   # erro quadrático médio
+
+    # Execulta a filtragem adaptativa
+    for n in range(N):
+
+        # janela deslizante para valores do sinal recebido
+        x = np.flipud(u[n:n+taps])
+
+        # calcula a saída no instante n
+        y[n] = np.dot(x, w)
+        
+        # calcula o erro ajustando o atraso da filtragem FIR
+        e[n] = d[n+(taps-1)//2] - y[n]
+        
+        # calcula os novos coeficientes do filtro
+        w += mu * x * e[n]
+
+        # calcula o erro quadrático
+        squaredError[n] = e[n]**2
+    
+    mse = squaredError/N
+
+    return y, e, mse, w
--- algorithms/frontEndCorrection.py
+++ algorithms/frontEndCorrection.py
@@ -3,7 +3,6 @@ import scipy as sp
 
 def convmtx(h, N):
     """
-    Realiza a implementação da função nativa presente no matlab.
     Determina uma matriz de convolução a partir de um vetor de entrada 'h'
 
     Args:
@@ -15,6 +14,27 @@ def convmtx(h, N):
     """
     H = sp.linalg.toeplitz(np.hstack((h, np.zeros(N-1))), np.zeros(N))
     return H.T
+
+def gsop(rLn):
+    """ Esta função realiza a ortogonalização de Gram-Schmidt no sinal 'rLn'
+
+    Args:
+        rLn (np.array): Sinal de entrada no qual será realizada a ortogonalização.
+    Returns:
+        sigRx (np.array): Sinal após ortogonalização de Gram-Schmidt.
+    """
+    # A 1ª e 2ª colunas devem conter as componentes em fase e quadratura do sinal, respectivamente.
+    Rin = np.array([rLn.real, rLn.imag]).T
+    
+    # Tomando como referência a componente em quadratura:
+    rQOrt = Rin[:,1]/np.sqrt(np.mean(Rin[:,1]**2))
+    # Orthogonalization:    
+    rIInt = Rin[:,0]-np.mean(Rin[:,1]*Rin[:,0])*Rin[:,1]/np.mean(Rin[:,1]**2)
+    rIOrt = rIInt/np.sqrt(np.mean(rIInt**2))
+
+    sigRx = rIOrt + 1j*rQOrt
+
+    return sigRx
 
 def Deskew(rIn, SpS, Rs, N, ParamSkew):
     """
@@ -49,7 +69,7 @@ def Deskew(rIn, SpS, Rs, N, ParamSkew):
     # Parte inteira e fracionária da inclinação.
     nTADC = np.floor(Skew);  muTADC = -(Skew-nTADC)
 
-    # Obtendo o filtro FIR e interpolando os sinais
+    # Obtendo o filtro FIR e realizando a interpolação.
     NTaps = N+1
     interp = []
 
@@ -73,5 +93,54 @@ def Deskew(rIn, SpS, Rs, N, ParamSkew):
     
     sigRx = np.array(interp)
     sigRx = sigRx[0,:] + 1j*sigRx[1,:]
+
+    return sigRx
+
+def InsertSkew(In, SpS, Rs, ParamSkew):
+    """ 
+    Esta função insere um desalinhamento temporal (inclinação) 
+    entre os componentes em fase e quadratura do sinal 'In'. O sinal 'In' 
+    deve ser o sinal obtido na saída do front-end óptico, logo antes do 'ADC'.
+    Nesta função, um atraso temporal é especificado para cada componente em 'ParamSkew'. 
+    O desalinhamento temporal entre os componentes é então aplicado assumindo o 
+    atraso temporal mínimo como referência.
+
+    Args:
+        In (np.array): Sinal na saída do front-end óptico.
+        SpS (int): Amostras por símbolo.
+        Rs (int): Taxa de símbolos [símbolo/s]
+        
+        ParamSkew (struct): Especifica o atraso temporal (em segundos) para cada componente do sinal de entrada.
+
+            - ParamSkew.TauIV: Atraso temporal para componente em fase.
+            - ParamSkew.TauQV: Atraso temporal para componente em quadratura.
+            
+    Returns:
+        np.array: Sinal produzido após inserção distorcida.
+    """
+    Rin = np.array([In.real, In.imag]).T
+    
+    iIV = Rin[:,0]
+    iQV = Rin[:,1]
+    
+    Ts = 1/(SpS*Rs)
+    Skew = np.array([ParamSkew.TauIV/Ts, ParamSkew.TauQV/Ts])
+    
+    # Usando a inclinação mínima como referência
+    Skew = Skew - np.min(Skew)
+    
+    # função de interpolação para quadratura e fase
+    interpI = sp.interpolate.CubicSpline(np.arange(0, len(iIV)), iIV, extrapolate=True)
+    interpQ = sp.interpolate.CubicSpline(np.arange(0, len(iQV)), iQV, extrapolate=True)
+
+    # novo eixo de interpolação 
+    inPhase    = np.arange(Skew[0], len(iIV))
+    quadrature = np.arange(Skew[1], len(iQV))
+
+    iIV = interpI(inPhase)
+    iQV = interpQ(quadrature)
+
+    MinLength = np.min([len(iIV), len(iQV)])
+    sigRx = iIV[:MinLength] + 1j*iQV[:MinLength]
 
     return sigRx
