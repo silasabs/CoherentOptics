138893097988038ca7a07cb4dbeb8b5345bbdb24
Silas João <silasroot321@gmail.com>
2024-06-24 11:48:35 -0300

    Update equalization.py

--- algorithms/equalization.py
+++ algorithms/equalization.py
@@ -189,10 +189,14 @@ def mimoAdaptEq(x, constSymb, paramEq):
     Args:
         x (np.array)        : sinal de entrada com duas polarizações
         constSymb (np.array): símbolos da constelação normalizados
+        
         paramEq (struct)    : 
             - paramEq.taps: número de coeficientes dos filtros.
+            
             - paramEq.lr  : tamanho do passo para a convergência do algoritmo.
+            
             - paramEq.alg : algoritmo de equalização adaptativa a ser usado: ['cma', 'rde'].
+            
             - paramEq.N   : número de cálculos de coeficientes a serem realizados antes da inicialização
                             adequada dos filtros w2H e w2V
 
