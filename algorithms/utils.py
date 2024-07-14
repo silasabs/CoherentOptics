eaf0862f137ef28dfa1c6c62aace6067ea623c32
Silas João <silasroot321@gmail.com>
2024-07-14 10:41:43 -0300

    Update utils.py

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -6,13 +6,20 @@ def convmtx(h, N):
     """
     Determina uma matriz de convolução a partir de um vetor de entrada 'h'
 
-    Args:
-        h (np.array): Vetor de entrada, especificado como linha ou coluna.
-        N (int): Comprimento do vetor a ser convoluído, especificado como um número inteiro positivo.
+    Parameters
+    ----------
+    h : np.array
+        matriz de entrada, especificado como linha ou coluna.
+    
+    N : int
+        Comprimento do vetor a ser convoluído, especificado como um número inteiro positivo.
 
-    Returns:
-        np.array: Retorna a matriz de convolução H
+    Returns
+    -------
+    H: np.array:
+        Matriz de convolução H
     """
+
     H = sp.linalg.toeplitz(np.hstack((h, np.zeros(N-1))), np.zeros(N))
     return H.T
 
