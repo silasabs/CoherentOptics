63194a8dad577765d55125c8f41904800c84408d
Silas João <silasroot321@gmail.com>
2024-10-05 21:56:06 -0300

    Update clockRecovery.py

--- algorithms/clockRecovery.py
+++ algorithms/clockRecovery.py
@@ -31,7 +31,7 @@ def interpolator(x, mu):
         x[3] * (1/6 * mu**3 + 1/2 * mu**2 + 1/3 * mu + 0)
     )
 
-def gardnerTED(x, PSType):
+def gardnerTED(x, pulseType):
     """
     Gardner TED Algorithm.
 
@@ -40,7 +40,7 @@ def gardnerTED(x, PSType):
     x : np.array
         Matriz de três amostras para o calculo do erro.
 
-    PSType : string
+    pulseType : string
         Tipo de pulso utilizado.
 
     Returns
@@ -54,7 +54,7 @@ def gardnerTED(x, PSType):
 
         [2] F. Gardner, A BPSK/QPSK timing-error detector for sampled receivers. IEEE Trans. Commun. 34(5), 423–429 (1986)
     """
-    if PSType == "Nyquist":
+    if pulseType == "Nyquist":
         ek = np.abs(x[1]) ** 2 * (np.abs(x[0]) ** 2 - np.abs(x[2]) ** 2)
     else:
         ek = np.real(np.conj(x[1]) * (x[2] - x[0]))
